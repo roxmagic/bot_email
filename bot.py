8b45039f14ca7d77efda1c20844f3c62dc99305b
roxmagic <kalebvosovepek@gmail.comProfile image>
2024-09-25 17:49:29 +0300

    added button

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import telebot
 import random
+from telebot import types
 
 bot = telebot.TeleBot('7934382999:AAF7tHgUfTaLHEhUxnSohqo_etpLSHso4S8')
 
@@ -25,18 +26,17 @@ email_list = [
 user_emails = {}
 
 # Функция для выдачи случайного email пользователю
-@bot.message_handler(commands=['getmail'])
 def get_random_email(message):
     global email_list, user_emails
     user_id = message.from_user.id
-    
+
     # Проверяем, есть ли еще доступные email
     if len(email_list) > 0:
         # Выбираем случайный email из доступных
         email = random.choice(email_list)
         email_list.remove(email)  # Убираем email из общего списка
         user_emails[user_id] = email  # Сохраняем выданный email для этого пользователя
-        
+
         # Отправляем пользователю выданный email
         bot.send_message(message.chat.id, f'Email для вас: `{email}`', parse_mode='MarkDown')
     else:
@@ -50,23 +50,38 @@ def reset_all_emails(message):
 
     # Восстанавливаем все email-адреса
     email_list = [
-    'lida.savchenko.93@mail.ru:fq5VGS)q87VUB:Лидия:Савченко',
-    'Shevchenkoenhx1982-oy@mail.ru:58QJVumaWY$H:Вера:Шевченко',
-    'inna.sabirova.99@mail.ru:)0CfWMO$xgeLy:Инна:Сабирова',
-    'ViktoriiaShakirovabafv@mail.ru:Oyq)4UBMb11p(:Виктория:Шакирова',
-    'LiubovVlasovashah@mail.ru:BSYM5L_)v)p)j:Любовь:Власова',
-    'Medvedevatflg.n.u@mail.ru:dos7MlUDi(B4z:Ольга:Медведева',
-    'Tarasovagmns1992.ul@mail.ru:GMe!rJoiXc0tw:Тамара:Тарасова',
-    'Mariiasgpdwi@mail.ru:Aq$A(1KZZ1g5:Мария:Карпенко',
-    'Fadeevawojc.q-x@mail.ru:VDk0evUdaK)LJ:Ольга:Фадеева',
-    'ViktoriiaRomanovaftix@mail.ru:VWi4_U45hrz1u:Виктория:Романова',
-    'NinaZhuravlevaxarg@mail.ru:FXE_)ted_P0I:Нина:Журавлева',
-    'Savchenko43.i@mail.ru:eRA699j)GzDBu:Валерия:Савченко',
-    'Liudmiladdxjth@bk.ru:S4hO0Ac$cdf7:Людмила:Лаптева',
-    'Marinaqacljz@list.ru:cVunbL)yc$2O:Марина:Лифанова',
+        'lida.savchenko.93@mail.ru:fq5VGS)q87VUB:Лидия:Савченко',
+        'Shevchenkoenhx1982-oy@mail.ru:58QJVumaWY$H:Вера:Шевченко',
+        'inna.sabirova.99@mail.ru:)0CfWMO$xgeLy:Инна:Сабирова',
+        'ViktoriiaShakirovabafv@mail.ru:Oyq)4UBMb11p(:Виктория:Шакирова',
+        'LiubovVlasovashah@mail.ru:BSYM5L_)v)p)j:Любовь:Власова',
+        'Medvedevatflg.n.u@mail.ru:dos7MlUDi(B4z:Ольга:Медведева',
+        'Tarasovagmns1992.ul@mail.ru:GMe!rJoiXc0tw:Тамара:Тарасова',
+        'Mariiasgpdwi@mail.ru:Aq$A(1KZZ1g5:Мария:Карпенко',
+        'Fadeevawojc.q-x@mail.ru:VDk0evUdaK)LJ:Ольга:Фадеева',
+        'ViktoriiaRomanovaftix@mail.ru:VWi4_U45hrz1u:Виктория:Романова',
+        'NinaZhuravlevaxarg@mail.ru:FXE_)ted_P0I:Нина:Журавлева',
+        'Savchenko43.i@mail.ru:eRA699j)GzDBu:Валерия:Савченко',
+        'Liudmiladdxjth@bk.ru:S4hO0Ac$cdf7:Людмила:Лаптева',
+        'Marinaqacljz@list.ru:cVunbL)yc$2O:Марина:Лифанова',
     ]
     user_emails.clear()  # Очищаем список выданных email
 
     bot.send_message(message.chat.id, 'Все email-адреса сброшены и готовы к новой выдаче.', parse_mode='html')
+
+# Обработчик команды /start и создание кнопок
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn_main = types.KeyboardButton('Отримати e-mail')
+    btn_delete = types.KeyboardButton('Видалити e-mail')
+    markup.row(btn_main)
+    bot.send_message(message.chat.id, 'Привет! Нажми на кнопку, чтобы получить e-mail.', reply_markup=markup)
+
+# Обработчик нажатий на кнопки
+@bot.message_handler(func=lambda message: True)
+def handle_buttons(message):
+    if message.text == 'Отримати e-mail':
+        get_random_email(message)
 
 bot.polling(none_stop=True)
